de4c16bb62c63a854d8f50c5b5215b71bd423077
Ibrahim <Ibrahim@ebs.net.pk>
2023-12-01 17:35:20 +0100

    changed navigator push page

--- lib/main.dart
+++ lib/main.dart
@@ -36,7 +36,7 @@ class _MyHomePageState extends State<MyHomePage> {
   void _sendToBarcodeScanPage() {
     Navigator.push(
       context,
-      MaterialPageRoute(builder: (context) => const Home()),
+      MaterialPageRoute(builder: (context) => const BarcodeScanPage()),
     );
   }
 
