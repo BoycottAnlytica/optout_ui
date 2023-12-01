7f0270703dfcf5ea59c1a1084224d0dacca977d3
Ibrahim <Ibrahim@ebs.net.pk>
2023-12-01 17:44:57 +0100

    added const

--- lib/main.dart
+++ lib/main.dart
@@ -47,7 +47,7 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: Center(
+      body: const Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
         ),
